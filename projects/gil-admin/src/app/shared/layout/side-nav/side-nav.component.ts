6eecc5f27b361a82154f13315aea1f2a68fb6e06
Luis Chang <luis.chang@outlook.com>
2022-12-02 16:10:00 -0500

    Add router link to side nav

--- projects/gil-admin/src/app/shared/layout/side-nav/side-nav.component.ts
+++ projects/gil-admin/src/app/shared/layout/side-nav/side-nav.component.ts
@@ -11,32 +11,38 @@ export class SideNavComponent implements OnInit {
     {
       number: '1',
       name: 'Home',
-      icon: 'fa-solid fa-house'
+      icon: 'fa-solid fa-house',
+      route: 'home'
     },
     {
       number: '2',
       name: 'Analytics',
-      icon: 'fa-solid fa-chart-line'
+      icon: 'fa-solid fa-chart-line',
+      route: 'analytics'
     },
     {
       number: '3',
       name: 'Products',
-      icon: 'fa-solid fa-box'
+      icon: 'fa-solid fa-box',
+      route: 'products'
     },
     {
       number: '4',
       name: 'Order',
-      icon: 'fa-solid fa-cart-shopping'
+      icon: 'fa-solid fa-cart-shopping',
+      route: 'order'
     },
     {
       number: '5',
       name: 'Settings',
-      icon: 'fa-solid fa-gear'
+      icon: 'fa-solid fa-gear',
+      route: 'settings'
     },
     {
       number: '6',
       name: 'About',
-      icon: 'fa-solid fa-circle-info'
+      icon: 'fa-solid fa-circle-info',
+      route: 'about'
     },
     {
       number: '7',
